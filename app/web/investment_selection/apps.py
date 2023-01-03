90f3bee786c12943e8f5e63c010d260d44626111
tkBischoff <tkbischoff@web.de>
2023-01-03 13:02:08 +0100

    Added authentification

--- app/web/investment_selection/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InvestmentSelectionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'investment_selection'
--- app/web/web/settings.py
+++ app/web/web/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login redirection
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
