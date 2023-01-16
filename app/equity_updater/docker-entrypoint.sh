695f469400490b190eba3679be4825e9fadbc01a
tkBischoff <tkbischoff@web.de>
2023-01-16 19:49:59 +0100

    Added entrypoints to run database migrations

---
+++ app/equity_updater/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Flush the manage.py command it any"
+
+while ! python manage.py flush --no-input 2>&1; do
+  echo "Flusing django manage command"
+  sleep 3
+done
+
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and run db migraiton
+while ! python manage.py makemigrations  2>&1; do
+   echo "Making migrations"
+   sleep 3
+done
+
+while ! python manage.py migrate  2>&1; do
+   echo "Migration is in progress status"
+   sleep 3
+done
+
+echo "Django docker is fully configured successfully."
+
+exec "$@"
